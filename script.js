9f9ddceb8039440a218f7c08d2891c2b8c287ce1
petrancirl <60797713+petrancirl@users.noreply.github.com>
2020-04-26 18:49:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+
+//RESPONSIVE NAVBAR 
+$(document).ready(function(){
+	$('.menu-toggle').click(function(){
+		$('nav').toggleClass('active')
+	})
+})
+
+
+//SLIDES DISCOVER
+
+var slideIndex = 1;
+
+var myTimer;
+
+var slideshowContainer;
+
+window.addEventListener("load",function() {
+    showSlides(slideIndex);
+    myTimer = setInterval(function(){plusSlides(1)}, 3000);
+    slideshowContainer = document.getElementsByClassName('slides')[0];
+    slideshowContainer.addEventListener('mouseenter', pause)
+    slideshowContainer.addEventListener('mouseleave', resume)
+})
+
+function plusSlides(n){
+  clearInterval(myTimer);
+  if (n < 0){
+    showSlides(slideIndex -= 1);
+  } else {
+   showSlides(slideIndex += 1); 
+  }
+  
+  if (n === -1){
+    myTimer = setInterval(function(){plusSlides(n + 2)}, 3000);
+  } else {
+    myTimer = setInterval(function(){plusSlides(n + 1)}, 3000);
+  }
+}
+
+function currentSlide(n){
+  clearInterval(myTimer);
+  myTimer = setInterval(function(){plusSlides(n + 1)}, 3000);
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+  var i;
+  var slides = document.getElementsByClassName("slider");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" activeActive", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " activeActive";
+}
+
+pause = () => {
+  clearInterval(myTimer);
+}
+
+resume = () =>{
+  clearInterval(myTimer);
+  myTimer = setInterval(function(){plusSlides(slideIndex)}, 3000);
+}
+
+//QUOTES BOTTOM
+
+setInterval(function() {
+  
+  var $quotes = $('blockquote'),
+      $quoteIn = $('blockquote.fade-in'),
+      $quoteOut = $quoteIn.next();
+	  
+  
+  $quoteOut = $quoteOut.length > 0 ? $quoteOut : $quotes.eq(0);
+  
+  $quoteIn.removeClass('fade-in').addClass('fade-out');
+  $quoteOut.removeClass('fade-out').addClass('fade-in');
+  
+  
+}, 6000);
+
+$('blockquote').eq(0).addClass('fade-in');
+
+
+
+
